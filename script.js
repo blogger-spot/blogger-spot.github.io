4277e4900932ef59b37d3cce0d9e4c5a08d74d93
Newbie Citizen <167607979+newbie-citizen@users.noreply.github.com>
2024-11-30 19:44:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-
+function owl_carousel (element, reference, gap = 0) {
+	var oc = $ (element);
+	if (oc) {
+		oc.width ($ (reference).width () - gap);
+		oc.owlCarousel ({
+			loop: true,
+			margin: 10,
+			nav: true,
+			autoplay: true,
+			autoplayTimeout: 5000,
+			autoplayHoverPause: true,
+			responsive: {
+				0: {items: 1},
+				600: {items: 3},
+				1000: {items: 5},
+				},
+			});
+		}
+	}
