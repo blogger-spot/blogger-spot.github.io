f41ccaa36fe7c24ddfaf6197b5752923ff6fc5b9
Newbie Citizen <167607979+newbie-citizen@users.noreply.github.com>
2024-12-29 19:11:25 +0700

    Update script.js

--- theme/wetube/script.js
+++ theme/wetube/script.js
@@ -101,11 +101,13 @@ function owl_carousel_odbc (context, url) {
 
 function body_class () {
 	var type = 'phone';
+	var orientation = 'portrait';
 	var body = $ ('body').innerWidth ();
-	if (body > 600) type = 'tablet';
+	if (body > 600) type = 'phone';
 	if (body > 1000) type = 'computer';
+	if ($ ('body').width () > $ ('body').height ()) orientation = "landscape";
 	$ ('body').removeClass ('computer mobile tablet phone');
-	$ ('body').addClass (type);
+	$ ('body').addClass (type).addClass (orientation);
 	}
 
 function body_click (event) {
