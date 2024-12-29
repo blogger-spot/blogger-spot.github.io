a602131dc79d2682a6686fedf96861a61303810a
newbie-citizen <newbie.citizen@gmail.com>
2024-12-30 00:00:00 +0700

    .

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,4 +1,40 @@
 $.odbc = {}
+$.label = {
+	name: {
+		"top-global": "Top Global",
+		"top-10": "Top 10",
+		"trending": "Trending",
+		"featured": "Featured",
+		"popular": "Popular",
+		"editor-choice": "EditorChoice",
+		"movie": "Movie",
+		"drama": "Drama",
+		"tv-show": "TV Show",
+		"playlist": "Playlist",
+		"episode": "Episode",
+		"group": "Group",
+
+		"international": "International",
+		"united-state": "United State",
+		"united-kingdom": "United Kingdom",
+		"france": "France",
+		"russia": "Russia",
+		"spain": "Spain",
+		"germany": "Germany",
+		"brazil": "Brazil",
+		"asia": "Asia",
+		"arab": "Arab",
+		"india": "India",
+		"indonesia": "Indonesia",
+		"malaysia": "Malaysia",
+		"korea": "Korea",
+		"china": "China",
+		"japan": "Japan",
+		"thailand": "Thailand",
+		"vietnam": "Vietnam",
+		"philippine": "Philippine",
+		},
+	}
 
 function owl_carousel (element, reference, option) {
 	option = option || {}
